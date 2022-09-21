Use numpy and fix calculation of radius

CNode.py
@@ -6,17 +6,19 @@ class CNode:
     def __init__(self, numWeights):
         self.dWeights = np.random.normal(0, 1, numWeights)
         self.PNodes = []
-
+        self.represent_vector=None
 
     def CalculateDistance(self, InputNode):
-        distance = 0
-        for i in range(len(InputNode)):
-            distance += (InputNode[i] - self.dWeights[i]) * (InputNode[i] - self.dWeights[i])
-        return distance
+        temp = self.dWeights - InputNode
+        sum_sq = np.dot(temp.T, temp)
+        return sum_sq
 
     def AdjustWeights(self, target, LearningRate, Influence):
-        for i in range(len(target)):
-            self.dWeights[i] += LearningRate * Influence * (target[i] - self.dWeights[i])
+            self.dWeights += LearningRate * Influence * (target - self.dWeights)
 
     def addPNode(self, corpus, inputPNode):
         self.PNodes.append(PNode(corpus, inputPNode))
+        return
+    
+    def __str__(self):
+      return np.array2string(self.dWeights)

CSom.py
@@ -7,54 +7,74 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from matplotlib import pyplot as plt
 
+def identity_tokenizer(text):
+    return text
+
 
 class CSom:
-    def __init__(self, MapSize, corpus, numIterations, constStartLearningRate=0.1):
+    def __init__(self, MapSize, corpus, numIterations, constStartLearningRate=0.5):
         self.MapSize = MapSize
-        self.corpus = copy.copy(corpus)
+        self.corpus = corpus
         self.numIterations = numIterations
         self.dMapRadius = MapSize / 2
         self.dTimeConstant = numIterations / math.log(self.dMapRadius)
         self.dLearningRate = constStartLearningRate
-        PNodes = TfidfVectorizer()
+        self.constLearningRate = constStartLearningRate
+        print("Start TFIDF")
+        PNodes = TfidfVectorizer(tokenizer=identity_tokenizer, lowercase=False)
         self.PNodes = PNodes.fit_transform(corpus).todense()
+        print("Done TFIDF")
+        print(self.PNodes[1])
         NodeDimension = self.PNodes.shape[1]
         self.m_Som = np.asarray([[CNode(NodeDimension) for j in range(MapSize)] for i in range(MapSize)])
 
     def FindBestMatchingNode(self, inputPNode):
         LowestDistance = 999999
+        SecDistance = 999999
         winner = None
         PNode = np.squeeze(np.asarray(inputPNode))
         for iy, ix in np.ndindex(self.m_Som.shape):
             dist = self.m_Som[iy, ix].CalculateDistance(PNode)
             if dist < LowestDistance:
-                if len(self.m_Som[iy, ix].PNodes) > 0:
-                    totalSim = 0
-                    for i in range(len(self.m_Som[iy, ix].PNodes)):
-                        totalSim += int(cosine_similarity(inputPNode, self.m_Som[iy, ix].PNodes[i].vector))
-                    if totalSim / len(self.m_Som[iy, ix].PNodes) < 0.5:
-                        continue
+                # if len(self.m_Som[iy, ix].PNodes) > 0:
+                #     totalSim = 0
+                #     for i in range(len(self.m_Som[iy, ix].PNodes)):
+                #         totalSim += int(cosine_similarity(inputPNode, self.m_Som[iy, ix].PNodes[i].vector))
+                #     if totalSim / len(self.m_Som[iy, ix].PNodes) < 0.5:
+                #         continue
                 LowestDistance = dist
                 winner = self.m_Som[iy, ix]
-        return winner
+                win_x=ix
+                win_y=iy
+        return winner, ix, iy
 
     def Train(self):
+        print("Start Training")
         for i in range(self.numIterations):
+            print(f"Epoch {i}")
             randomPNode = int(np.random.randint(self.PNodes.shape[0], size=1))
-            WinningNode = self.FindBestMatchingNode(self.PNodes[randomPNode])
+            WinningNode, grid_x, grid_y = self.FindBestMatchingNode(self.PNodes[randomPNode])
             dNeighbourhoodRadius = self.dMapRadius * math.exp(-float(i) / self.dTimeConstant);
             WidthSq = dNeighbourhoodRadius * dNeighbourhoodRadius
             for iy, ix in np.ndindex(self.m_Som.shape):
-                DistToNodeSq = self.m_Som[iy, ix].CalculateDistance(WinningNode.dWeights)
+                DistToNodeSq = (iy-grid_y)*(iy-grid_y)+(ix-grid_x)*(ix-grid_x)
 
-                if DistToNodeSq < dNeighbourhoodRadius ** 2:
+                if True:
                     self.dInfluence = math.exp(-(DistToNodeSq) / (2 * WidthSq))
                     self.m_Som[iy, ix].AdjustWeights(np.squeeze(np.asarray(self.PNodes[randomPNode])),
                                                      self.dLearningRate, self.dInfluence)
-            self.dLearningRate = self.dLearningRate * math.exp(-float(i) / (self.numIterations - i))
-        for i in range(self.PNodes.shape[0]):
-            SuitNode = self.FindBestMatchingNode(self.PNodes[i])
-            SuitNode.addPNode(self.corpus[i], self.PNodes[i])
+            self.dLearningRate = self.constLearningRate * math.exp(-float(i) / (self.dTimeConstant))
+            if i%5==0:
+              with open('ksom.txt', 'w') as f:
+                for iy, ix in np.ndindex(self.m_Som.shape):
+                  f.write(f"{iy} {ix} {self.m_Som[iy,ix]} \n")
+
+
+        # for i in range(self.PNodes.shape[0]):
+        #     SuitNode = self.FindBestMatchingNode(self.PNodes[i])
+        #     # print(self.PNodes[i])
+        #     # print(SuitNode)
+        #     SuitNode.addPNode(self.corpus[i], self.PNodes[i])
 
     def Plot(self):
         plt.rcParams["figure.autolayout"] = True

run.py
@@ -1,22 +1,103 @@
 import pymongo
 from CSom import *
+import math
+import numpy as np
+import string
+import pymongo
+import re
+import sys
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# client = pymongo.MongoClient("mongodb+srv://fbfighter:fbfighter@fb-topic.ixbkp2u.mongodb.net/?retryWrites=true&w=majority")
-#
-# db = client["fakenews"]
-#
-# col = db["fbpost"]
-#
-# data = col.find()
-#
-# train_set = []
+import unicodedata as ud
 
-corpus = ['The sun is the largest celestial body in the solar system',
-          'The solar system consists of the sun and eight revolving planets',
-          'Ra was the Egyptian Sun God',
-          'The Pyramids were the pinnacle of Egyptian architecture',
-          'The quick brown fox jumps over the lazy dog']
+client = pymongo.MongoClient("mongodb+srv://fbfighter:fbfighter@fb-topic.ixbkp2u.mongodb.net/?retryWrites=true&w=majority")
+
+db = client["fakenews"]
+
+col = db["fbpost"]
+
+data = col.find()
+
+corpus = []
+
+letters = string.ascii_lowercase
+
+print("Start preprocess")
+for x in data:
+    corpus.append(x['text'])
+
+def preprocess_text(text):
+    special_char = ['̼', '“', '”', '–']
+    punctualtion_list = string.punctuation + "".join(special_char)
+    removed_punctuation = "".join([i for i in text if i not in punctualtion_list])
+    lower_case = removed_punctuation.lower()
+    # translated_text = lower_case.translate(translator)
+    # return translated_text
+    
+    return lower_case
+
+# for i in clean_text:
+#     print("post-----------")
+#     print(i)
+
+def find_special_char(text):
+    regex = "[^a-z0-9A-Z_\sÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễếệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]"
+    set_char = set(re.findall(regex, text))
+    return set_char
+
+def replace_special_char(text):
+    mapping_list = {}
+    set_char = find_special_char(text)
+    for char in set_char:
+        try:
+            char_name = ud.name(char)
+            letter = char_name.split(' ')[-1].lower()
+            if letter in string.ascii_lowercase:
+                mapping_list[ord(char)] = letter
+        except:
+            pass
+    return text.translate(str.maketrans(mapping_list))
+
+clean_text = list(map(preprocess_text, corpus))
+clean_text2 = list(map(replace_special_char, clean_text))
 
 
-model = CSom(100, corpus, 10)
+
+def tokenization(text):
+    tokens = re.split('\s+', text)
+    return tokens
+
+token_text_list = list(map(tokenization, clean_text2))
+
+print("Done Preprocess")
+
+def identity_tokenizer(text):
+    return text
+
+
+model = CSom(26, token_text_list, 500)
 model.Train()
+# PNodes = TfidfVectorizer()
+# PNodes = PNodes.fit_transform(corpus_val).todense()
+
+# for i in range(PNodes.shape[0]):
+#     SuitNode, _, _ = model.FindBestMatchingNode(PNodes[i])
+#     # print(PNodes[i])
+#     # print(SuitNode)
+#     SuitNode.addPNode(corpus_val[i], PNodes[i])
+
+PNodes = TfidfVectorizer(tokenizer=identity_tokenizer, lowercase=False)
+PNodes = PNodes.fit_transform(token_text_list).todense()
+sum_quan_err=0
+for i in range(PNodes.shape[0]):
+    SuitNode, _, _ = model.FindBestMatchingNode(PNodes[i])
+    sum_quan_err+=math.sqrt(SuitNode.CalculateDistance(np.squeeze(np.asarray(PNodes[i]))))
+    # print(PNodes[i])
+    # print(SuitNode)
+    SuitNode.addPNode(token_text_list[i], PNodes[i])
+print(f"Quantization Error {sum_quan_err/len(token_text_list)}")
+# for iy, ix in np.ndindex(model.m_Som.shape):
+#     for i in range(len(model.m_Som[iy,ix].PNodes)):
+#         print(iy," ",ix," ",model.m_Som[iy,ix].PNodes[i].corpus)
+# model.Plot()